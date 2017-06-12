e0daed4c91c95008a93221d9c7e87bcd682e8e67
OlgaTom <oliatomilova@gmail.com>
2017-06-12 13:21:51 +0300

    first commit

---
+++ task6/scripts.js
@@ -0,0 +1,14 @@
+	
+
+	var w;
+
+	var lesson1 = document.getElementById("lesson1").onclick = function(){
+		w = window.open("lesson1.html", "JS Урок 1", "width=600, height=400, status=yes, resizable=no");
+	};
+
+	var lesson2 = document.getElementById("lesson2").onclick = function(){
+		w = window.open("lesson2.html", "JS Урок 2", "width=600, height=400, status=yes, resizable=no");
+	};
+	var lesson3 = document.getElementById("lesson3").onclick = function(){
+		w= window.open("lesson3.html", "JS Урок 3", "width=600, height=400, status=yes, resizable=no");
+	};
